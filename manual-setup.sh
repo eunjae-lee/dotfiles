a20386d6819ebbffa79a2445086bd5e6690671a5
Eunjae Lee <karis612@gmail.com>
2021-09-14 13:43:59 +0200

    initial commit

---
+++ manual-setup.sh
@@ -0,0 +1,48 @@
+echo "Setting up SSH key for GitHub..."
+ssh-keygen -t rsa
+cat ~/.ssh/id_rsa.pub | pbcopy
+open "https://github.com/settings/ssh/new"
+
+echo "Installing xcode-stuff..."
+
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
+
+echo "Installing Git..."
+brew install git
+
+echo "Setting up dotfiles..."
+git clone git@github.com:eunjae-lee/dotfiles.git .dotfiles
+
+ln -s .dotfiles/.gitconfig .gitconfig
+
+echo "Setting up zsh..."
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+rm .zshrc
+ln -s ~/.dotfiles/.zshrc .zshrc
+
+echo "Setting up node.js..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+git clone https://github.com/lukechilds/zsh-nvm "$ZSH_CUSTOM/plugins/zsh-nvm" --depth=1
+
+brew install wget
+
+apps=(
+  firefox
+  google-chrome
+  spotify
+  iterm2
+  visual-studio-code
+)
+
+echo "installing apps with Cask..."
+brew install --cask ${apps[@]}
