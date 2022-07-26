6c19f45ee58d2f4edf1afe432cd01cc409e944a9
Eunjae Lee <karis612@gmail.com>
2022-07-26 17:53:21 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -60,7 +60,7 @@ echo "Setting up node.js..."
 # asdf
 # https://gist.github.com/Grawl/461c7c1acfcf7e2ecbf99ce9fed40c31
 brew install asdf
-echo ". /usr/local/opt/asdf/libexec/asdf.sh" >> ~/.zshrc
+# echo ". /usr/local/opt/asdf/libexec/asdf.sh" >> ~/.zshrc
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 brew install gnupg
 asdf install nodejs lts
