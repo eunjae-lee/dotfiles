fed3c4304ee6cb800d79074b0883044d6bdd80eb
Eunjae Lee <hey@eunjae.dev>
2023-08-03 17:55:15 +0200

    update

--- config/nvim/lua/user/plugins/community.lua
+++ config/nvim/lua/user/plugins/community.lua
@@ -7,4 +7,11 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.yaml" },
 }
