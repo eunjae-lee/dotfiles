aca8ea5350f3c7a88d278f81621d167beabd063d
Eunjae Lee <hey@eunjae.dev>
2023-08-11 15:53:03 +0200

    update

--- config/nvim/lua/user/plugins/user.lua
+++ config/nvim/lua/user/plugins/user.lua
@@ -25,4 +25,16 @@ return {
   {
     "LinArcX/telescope-command-palette.nvim",
   },
+  {
+    "Wansmer/treesj",
+    keys = {
+      {
+        "<leader>m",
+        "<CMD>TSJToggle<CR>",
+        desc = "Toggle Treesitter Join",
+      },
+    },
+    cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
+    opts = { use_default_keymaps = false },
+  },
 }
