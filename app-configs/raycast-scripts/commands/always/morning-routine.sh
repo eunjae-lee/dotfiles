24b004d3e69ba477ff0a4a7144c41d5d08bc9810
Eunjae Lee <hey@eunjae.dev>
2024-04-22 10:00:01 +0200

    update

---
+++ app-configs/raycast-scripts/commands/always/morning-routine.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Morning Routine
+# @raycast.mode compact
+
+cd /Users/eunjae/workspace/dotfiles/xc
+xc -f morning-routine.md next
