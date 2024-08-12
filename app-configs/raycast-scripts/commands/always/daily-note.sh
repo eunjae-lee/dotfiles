4baa74516a14d7498903764b72d0635343441ffa
Eunjae Lee <hey@eunjae.dev>
2024-08-12 16:00:00 +0200

    update

---
+++ app-configs/raycast-scripts/commands/always/daily-note.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# @raycast.title Open Daily Note
+#
+# @raycast.mode silent
+# @raycast.icon 🖥
+# @raycast.schemaVersion 1
+
+osascript -e 'tell application "System Events" to key code 105'
