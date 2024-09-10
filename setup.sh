8f8a31eb7d6d650d85740af13ac5100b6ebb00cc
Eunjae Lee <hey@eunjae.dev>
2024-09-10 09:27:35 +0200

    add python setup

--- setup.sh
+++ setup.sh
@@ -145,6 +145,11 @@ brew install xc
 # install one-thing CLI
 npm install -g one-thing
 
+# install python
+asdf plugin-add python
+asdf install python 3.11.3
+asdf global python 3.11.3
+
 # torch
 pip3 install --upgrade pip
 pip3 install torch torchvision
