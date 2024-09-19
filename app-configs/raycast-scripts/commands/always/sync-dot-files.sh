299fafa89498d411abf752a943834169f8104ee5
Eunjae Lee <hey@eunjae.dev>
2024-09-19 21:11:32 +0200

    update

--- app-configs/raycast-scripts/commands/always/sync-dot-files.sh
+++ app-configs/raycast-scripts/commands/always/sync-dot-files.sh
@@ -11,3 +11,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 (cd $SCRIPT_DIR && cd ../../../../ && git add . && git commit -m "update" && git pull --no-edit && git push)
+(cd $SCRIPT_DIR && cd ../../../../ && git pull)
