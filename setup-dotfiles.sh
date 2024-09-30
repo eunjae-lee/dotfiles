228d50639f4e249099e69a8e5bf3d3b362a63308
Eunjae Lee <hey@eunjae.dev>
2024-09-30 10:56:47 +0200

    .

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -8,3 +8,6 @@ ln -s ~/workspace/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/workspace/dotfiles/.tool-versions ~/.tool-versions
 
 ln -s ~/workspace/dotfiles/app-configs/raycast-scripts ~/workspace/raycast-scripts
+
+rm ~/Library/Application\ Support/lazygit/config.yml
+ln -s ~/workspace/dotfiles/app-configs/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
