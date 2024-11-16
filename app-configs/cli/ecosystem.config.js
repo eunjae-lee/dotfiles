522024cb399488b4cce4e8ed0598b43c0218138a
Eunjae Lee <hey@eunjae.dev>
2024-11-16 21:59:26 +0100

    use pm2

---
+++ app-configs/cli/ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: [
+    {
+      name: "eunjae-cli",
+      script: "/Users/eunjae/workspace/dotfiles/bin/ej watch",
+    },
+  ],
+};
--- setup-cli.sh
+++ setup-cli.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
-sudo cp ~/workspace/dotfiles/app-configs/cli/dev.eunjae.cli.plist /Library/LaunchDaemons
-sudo launchctl load -w /Library/LaunchDaemons/dev.eunjae.cli.plist
-sudo launchctl start -w /Library/LaunchDaemons/dev.eunjae.cli.plist
+npm install pm2@latest -g
+pm2 startup
+
+# sudo cp ~/workspace/dotfiles/app-configs/cli/dev.eunjae.cli.plist /Library/LaunchDaemons
+# sudo launchctl load -w /Library/LaunchDaemons/dev.eunjae.cli.plist
+# sudo launchctl start -w /Library/LaunchDaemons/dev.eunjae.cli.plist
 
 # sudo launchctl stop -w /Library/LaunchDaemons/dev.eunjae.cli.plist
 # sudo launchctl unload -w /Library/LaunchDaemons/dev.eunjae.cli.plist
