7b23aa0bef9977af222b38676e2c636a8711e02f
Eunjae Lee <hey@eunjae.dev>
2024-11-16 22:00:49 +0100

    .

--- setup-cli.sh
+++ setup-cli.sh
@@ -2,10 +2,3 @@
 npm install pm2@latest -g
 cd app-configs/cli && pm2 start ecosystem.config.js
 pm2 startup
-
-# sudo cp ~/workspace/dotfiles/app-configs/cli/dev.eunjae.cli.plist /Library/LaunchDaemons
-# sudo launchctl load -w /Library/LaunchDaemons/dev.eunjae.cli.plist
-# sudo launchctl start -w /Library/LaunchDaemons/dev.eunjae.cli.plist
-
-# sudo launchctl stop -w /Library/LaunchDaemons/dev.eunjae.cli.plist
-# sudo launchctl unload -w /Library/LaunchDaemons/dev.eunjae.cli.plist
