f97b9e8545c39792efff3cabdea443fabffd0ffb
Eunjae Lee <hey@eunjae.dev>
2025-10-01 09:45:57 +0900

    improve mas provider

--- dots/lib/providers/mas.rb
+++ dots/lib/providers/mas.rb
@@ -10,17 +10,18 @@ module Dots
         end
 
         config['apps'].each_with_index do |app, index|
-          unless app.is_a?(Hash)
-            errors << "MasProvider app at index #{index} must be a hash"
+          if app.is_a?(Integer) || (app.is_a?(String) && app.match?(/^\d+$/))
             next
-          end
+          elsif app.is_a?(Hash)
+            unless app['name'] && app['id']
+              errors << "MasProvider app at index #{index} must have 'name' and 'id'"
+            end
 
-          unless app['name'] && app['id']
-            errors << "MasProvider app at index #{index} must have 'name' and 'id'"
-          end
-
-          if app['id'] && !(app['id'].is_a?(Integer) || (app['id'].is_a?(String) && app['id'].match?(/^\d+$/)))
-            errors << "MasProvider app '#{app['name']}' must have numeric 'id'"
+            if app['id'] && !(app['id'].is_a?(Integer) || (app['id'].is_a?(String) && app['id'].match?(/^\d+$/)))
+              errors << "MasProvider app '#{app['name']}' must have numeric 'id'"
+            end
+          else
+            errors << "MasProvider app at index #{index} must be a hash with 'name' and 'id', or a numeric id"
           end
         end
 
@@ -31,13 +32,15 @@ module Dots
         check_mas_installed
 
         config['apps'].each do |app|
-          next if app_installed?(app['id'])
+          app_id, app_name = normalize_app(app)
+          
+          next if app_installed?(app_id)
 
-          puts "Installing app: #{app['name']} (#{app['id']})"
-          stdout, stderr, status = Open3.capture3("mas install #{app['id']}")
+          puts "Installing app: #{app_name} (#{app_id})"
+          stdout, stderr, status = Open3.capture3("mas install #{app_id}")
 
           unless status.success?
-            raise ApplyError, "Failed to install #{app['name']}: #{stderr}"
+            raise ApplyError, "Failed to install #{app_name}: #{stderr}"
           end
         end
 
@@ -45,11 +48,22 @@ module Dots
       end
 
       def describe
-        app_names = config['apps'].map { |app| app['name'] }.join(', ')
+        app_names = config['apps'].map do |app|
+          app_id, app_name = normalize_app(app)
+          app_name
+        end.join(', ')
         "Install #{config['apps'].length} Mac App Store app(s): #{app_names}"
       end
 
       private
+
+      def normalize_app(app)
+        if app.is_a?(Integer) || (app.is_a?(String) && app.match?(/^\d+$/))
+          [app.to_s, app.to_s]
+        else
+          [app['id'].to_s, app['name']]
+        end
+      end
 
       def check_mas_installed
         stdout, stderr, status = Open3.capture3('which mas')
